255a6d234f07fb10e55ce06e37bc845e4cd3655e
Jonathan Deates <jonathan.t.deates.ctr@mail.mil>
2024-06-20 11:26:04 -0500

    Updated slanted header.

--- apps/portfolio/src/pages/Home/Home.tsx
+++ apps/portfolio/src/pages/Home/Home.tsx
@@ -3,6 +3,7 @@ import './Home.css';
 import Introduction from "./components/Introduction/Introduction";
 import HOME_DATA from "./data/HomeData";
 import Section from "./components/Section/Section";
+import SlantedOutlinedHeader from "./components/SlantedOutlinedHeader/SlantedOutlinedHeader";
 
 const Home = () => {
   const minWidth = window.innerWidth < 767;
@@ -10,16 +11,29 @@ const Home = () => {
   return (
     <>
       <Introduction/>
-      <div className='TextStuffWrapper'>
-        <div className="TextStuff">
-          <p>TS</p>
-          <p>ReactJS</p>
-          <p>NextJs</p>
-          <p>ExpressJs</p>
-        </div>
-      </div>
+      <div className="WrapperText">
+        <SlantedOutlinedHeader
+          headerList={['TS','ReactJs', 'NextJs', 'CSS']}
+          outlineWidth="5px"
+          rotationAngle={-9}
+          fontSize={80}
+          backgroundColor={'rgb(153, 224, 255)'}
+          outerBackgroundColor={'aliceblue'}
+        />
       <div className={"FrontendStuff"}>
-        <h2>Frontend Expert</h2>
+        <h2>Frontend Nerd</h2>
+      </div>
+      </div>
+      <div >
+        <SlantedOutlinedHeader headerList={['Java', 'Spring', 'ExpressJs']}
+                               rotationAngle={9}
+                               outlineWidth='5px'
+                               outerBackgroundColor={'rgb(153,224,255)'}
+                               backgroundColor={'green'}
+                               fontSize={80}/>
+        <div className={'FrontendGarbage'}>
+          <h2>Backend Skillz</h2>
+        </div>
       </div>
       {/*{sectionList.map((section, i) =>*/}
       {/*  <Section key={'section-type-' + i} section={section}*/}
---
+++ apps/portfolio/src/pages/Home/components/SlantedOutlinedHeader/SlantedOutlinedHeader.tsx
@@ -0,0 +1,64 @@
+import React, {CSSProperties} from "react";
+import './SlantedOutlinedHeader.css';
+
+type SlantedOutlinedHeaderType = {
+  headerList: string[],
+  style?: CSSProperties,
+  styleOfWrapper?: CSSProperties,
+  rotationAngle?: number,
+  backgroundColor?: string,
+  outerBackgroundColor?: string,
+  outlineWidth?: string,
+  fontSize?: number,
+}
+
+const SlantedOutlinedHeader = ({
+                                 headerList,
+                                 style = {},
+                                 styleOfWrapper = {},
+                                 rotationAngle = -4,
+                                 backgroundColor = "grey",
+                                 outlineWidth = "4px",
+                               outerBackgroundColor = 'white',
+                                 fontSize = 40,
+                               }: SlantedOutlinedHeaderType) => {
+
+  const isRotationNegative = rotationAngle < 0;
+
+  const slantedOutlineHeaderStyle = {
+    ...style,
+    transform: `rotate(${rotationAngle}deg) translate(0, 30px)`,
+    color: outerBackgroundColor,
+    textShadow: `${outlineWidth} 0 0 ${backgroundColor}, 
+    0 ${outlineWidth} 0 ${backgroundColor}, 
+    -${outlineWidth} 0 0 ${backgroundColor}, 
+    0 -${outlineWidth} 0 ${backgroundColor}
+    `,
+    borderBottomColor: backgroundColor,
+    fontSize,
+    borderBottomWidth: `${fontSize * 5}px`,
+    marginBottom: `-${fontSize * 2.5}px`,
+    left: isRotationNegative ? 'unset' : '-2%',
+  };
+
+  const slatedOutlineWrapperStyle = {
+    ...styleOfWrapper,
+    backgroundColor: outerBackgroundColor
+  };
+
+  const slantedOutlineTextStyle = {
+   marginBottom: `-${fontSize * .5}px`
+  };
+
+  return <div className='SlantedOutlinedHeaderWrapper' style={slatedOutlineWrapperStyle}>
+    <div className='SlantedOutlinedHeader' style={slantedOutlineHeaderStyle}>
+      {headerList.map((header, index) =>
+        <p key={header + index} style={slantedOutlineTextStyle}>
+          {header}
+        </p>)
+      }
+    </div>
+  </div>
+};
+
+export default SlantedOutlinedHeader;
